cef56605fd805a73ed539379d99c944eba21de22
Adam Weaver <adamweaver90@gmail.com>
2022-09-10 13:18:25 -0400

    commit tree 522aabca0fbb5da0b36f698bb525c44212a58468

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+const guests = ["Guadalupe", "Ollie", "Aki"];
+const message = "surprise";
+
+function writeCards(names, surprise) {
+  console.log(names, surprise);
+   const newArray = [] 
+   
+    for (let i = 0; i < names.length; i++) {
+        const newString = `Thank you, ${names[i]}, for the wonderful ${surprise} gift!`;
+        newArray.push(newString);
+    }
+    return newArray;
+}
+//writeCards(guests, message);
+
+function countDown(number) {
+  let i = number;
+  while (i >= 0) {
+      
+    console.log(i);
+    i--;
+  }
+  return number;
+
+}
+countDown(40);
